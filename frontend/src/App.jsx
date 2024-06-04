2ebb03cf5926bd20481ce32624b3c2a6f34fec82
Harshsharma008 <harshsharma01012002@gmail.com>
2024-06-04 09:48:43 +0530

    nav&banner&footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./components/Navbar"
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Banner />
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App
